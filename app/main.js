2175f5783ce6dc10281c94255931bf032da96879
Zamariolo <vnsaugusto@gmail.com>
2020-03-06 03:00:03 +0000

    Tela do menu inicial terminada

--- app/main.js
+++ app/main.js
@@ -1,15 +1,19 @@
-const electron = require('electron');
-const app = electron.app;
-const windowManager = require('electron-window-manager');
+const {app, BrowserWindow} = require('electron');
+
+//Se utilizar mult window
+//const windowManager = require('electron-window-manager');
 
 let mainWindow = null;
 
 //Executa quando a aplicacao termina de carregar
 app.on('ready', function(){
-    windowManager.init();
-    //Open a window
-    mainWindow = windowManager.open('mainWindow', 'Originalll vidros', '/app/index.html',null,{
-        webPreferences: {nodeIntegration: true},
-        //Colocar outras configuracoes aqui
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
-})
+
+    mainWindow.webContents.loadFile('app/index.html');
+    mainWindow.removeMenu();
+});
+

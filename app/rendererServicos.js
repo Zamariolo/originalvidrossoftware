32a6c8c49f46738eb6d1d7293c1013e03cde3a1b
Zamariolo <vnsaugusto@gmail.com>
2020-05-26 11:40:24 -0300

    Inspec quase pronto |carregar, salvar, excluir OK

--- app/rendererServicos.js
+++ app/rendererServicos.js
@@ -1,9 +1,22 @@
 let renderer = require('./renderer.js');
 var {BrowserWindow} = require('electron').remote;
+var {dialog} = require('electron').remote;
+var fs = require('fs');
 
 let btnServicoMenu = document.getElementById('btnServicoMenu');
 btnServicoMenu.addEventListener('click', ()=>{renderer.trocaTela('janelaServicos', 'janelaMenu');});
 
+// Escondendo o inspec ao inicializar
+document.getElementById('div_inspec').style.display = 'none';
+
+// Botoes salvar e excluir do inspec
+document.getElementById('btnSalvar_inspec').addEventListener('click',()=>{salvarServico(document.getElementById('id_inspec').innerHTML);});
+document.getElementById('btnExcluir_inspec').addEventListener('click', ()=>{excluirServico(document.getElementById('id_inspec').innerHTML);});
+
+// Botao trocar status
+document.getElementById('btnTrocaStatus_1').addEventListener('click', ()=>{trocaStatus(document.getElementById('id_inspec').innerHTML,1);});
+document.getElementById('btnTrocaStatus_2').addEventListener('click', ()=>{trocaStatus(document.getElementById('id_inspec').innerHTML,2);});
+document.getElementById('btnTrocaStatus_3').addEventListener('click', ()=>{trocaStatus(document.getElementById('id_inspec').innerHTML,3);});
 
 function mostraServicos(dados_servicos){
     console.log('Mostra servicos funcionando!');
@@ -32,11 +45,13 @@ function mostraServicos(dados_servicos){
         html_lista_servicos = html_lista_servicos + 
         `<div class="item_servico row">
         <img src="${icone_status}" alt="" class="imagem_item_servico col-1 status" id='status_${dados_servicos[i].idServico}'>
-            <p class="col-1 lead texto_item_servico">${dados_servicos[i].idServico}</p>
+            <p class="col-1 lead texto_item_servico" id='idServico_${dados_servicos[i].idServico}'>${dados_servicos[i].idServico}</p>
             <p class="col-2 lead texto_item_servico">${dados_servicos[i].dataServico}</p>
-            <p class="col-5 lead texto_item_servico">${dados_servicos[i].nome}</p>
+            <p class="col-5 lead texto_item_servico" id='nomeCliente_${dados_servicos[i].idServico}'>${dados_servicos[i].nome}</p>
             <p class="col-2 lead texto_item_servico"><b>R$ ${dados_servicos[i].valorTotal}</b></p>
-            <img src="media/assets/comments.png" alt="" class="imagem_item_servico col-1 inspec" id='btn_inspec_${dados_servicos[i].idServico}'>
+            <img src="media/assets/comments.png" alt="" class="imagem_item_servico col-1 inspec" id='statusServico_${dados_servicos[i].idServico}'>
+            <p class="lead small" id='comentario_${dados_servicos[i].idServico}'>${dados_servicos[i].comentarios}</p>
+            <p id='status_num_${dados_servicos[i].idServico}' style='display: none'>${dados_servicos[i].status}</p>
         </div> <!-- Fecha div item-->
         `
     }
@@ -59,15 +74,119 @@ function mostraServicos(dados_servicos){
 }
 module.exports.mostraServicos = mostraServicos; //deixa func visivel pra renderer.js
 
-
 function abreInspec(id)
 {
-    console.log('Inspec aberto');
-    console.log(id);
+    // id numerico
+    console.log('id numerico:')
+    console.log(id.slice(14))
+    var id_num = id.slice(14);
+
+    // Abre inspec
+    document.getElementById('div_inspec').style.display = 'grid';
+    //Scroll para o topo
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    // Carrega dados ao inspec
+    document.getElementById('id_inspec').innerHTML = document.getElementById(`idServico_${id_num}`).innerHTML;
+    document.getElementById('nome_inspec').innerHTML = document.getElementById(`nomeCliente_${id_num}`).innerHTML;
+    document.getElementById('endereco_inspec').value = `${__dirname}\\app\\pdf_servicos\\serv_${id_num}.pdf`;
+    document.getElementById('comentarios_inspec').value = document.getElementById(`comentario_${id_num}`).innerHTML;
+    
+    console.log("Status:" + document.getElementById(`status_num_${id_num}`).innerHTML)
+    
+    switch (document.getElementById(`status_num_${id_num}`).innerHTML)
+    {
+        case '1':
+            document.getElementById("btnTrocaStatus_1").style.border = "2px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "0px solid black";
+            break;
+        case '2':
+            document.getElementById("btnTrocaStatus_1").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "2px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "0px solid black";
+            break;
+        case '3':
+            document.getElementById("btnTrocaStatus_1").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "2px solid black";
+            break;
+    }
+    // Recarrega servicos | atualiza tela
+
 }
 
-function trocaStatus(id)
+function atualizaLista()
+{
+    // Faz o intermedio entre mysql e a funcao nodejs
+    // Chamar esta funcao sempre que a lista de servicos precise ser atualizada
+    renderer.connection.query("SELECT servicos.idServico, DATE_FORMAT(servicos.dataServico, '%d-%m-%Y') as dataServico, servicos.valorTotal, servicos.comentarios, servicos.status, clientes.nome FROM servicos LEFT JOIN clientes ON servicos.idCliente = clientes.idCliente;", function(err, result, fields){mostraServicos(result)});
+}
+
+function salvarServico(id)
+{
+    renderer.connection.query(`UPDATE servicos set comentarios='${document.getElementById('comentarios_inspec').value}' where idServico='${id}'`);
+    // Limpa e fecha janela inspec
+    document.getElementById('id_inspec').innerHTML = '';
+    document.getElementById('nome_inspec').innerHTML = '';
+    document.getElementById('endereco_inspec').value = '';
+    document.getElementById('comentarios_inspec').value = '';
+    document.getElementById('div_inspec').style.display = 'none';
+    atualizaLista();
+}
+
+function excluirServico(id)
+{
+    const confirmOptions = {
+        type: 'warning',
+        defaultId: 0,
+        buttons: ['Tenho certeza!', 'Cancelar'],
+        title: 'Deletar serviço',
+        message: `Você tem certeza que deseja remover o serviço de id ${id} da base de dados?`,
+        detail: 'Essa ação não pode ser desfeita e o boleto será apagado'
+    };
+    let confirmacao = dialog.showMessageBoxSync(null, confirmOptions)
+    //Aceitou deletar
+    if (confirmacao == 0) {
+        renderer.connection.query(`DELETE FROM servicos where idServico='${id}'`);
+        fs.unlinkSync(`${__dirname}\\pdf_servicos\\serv_${id}.pdf`, function (err) {
+            if (err) throw err;
+            // if no error, file has been deleted successfully
+            console.log('File deleted!');
+        });
+        atualizaLista();
+    }
+
+    // Limpa e fecha janela inspec
+    document.getElementById('id_inspec').innerHTML = '';
+    document.getElementById('nome_inspec').innerHTML = '';
+    document.getElementById('endereco_inspec').value = '';
+    document.getElementById('comentarios_inspec').value = '';
+    document.getElementById('div_inspec').style.display = 'none';
+}
+
+function trocaStatus(id, paraStatus)
 {
     console.log('troca status');
+    console.log('paraStatus' + String(paraStatus));
     console.log(id);
+
+    switch (paraStatus)
+    {
+        case 1:
+            document.getElementById("btnTrocaStatus_1").style.border = "2px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "0px solid black";
+            break;
+        case 2:
+            document.getElementById("btnTrocaStatus_1").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "2px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "0px solid black";
+            break;
+        case 3:
+            document.getElementById("btnTrocaStatus_1").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_2").style.border = "0px solid black";
+            document.getElementById("btnTrocaStatus_3").style.border = "2px solid black";
+            break;
+    }
+
 }
